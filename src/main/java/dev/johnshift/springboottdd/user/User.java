fcee7bbe52f5989140d08dcec151bfcee160fbaa
John Ballesteros <johnshift.dev@gmail.com>
2021-09-26 10:50:56 +0800

    connected to prod db

--- src/main/java/dev/johnshift/springboottdd/user/User.java
+++ src/main/java/dev/johnshift/springboottdd/user/User.java
@@ -1,16 +1,31 @@
 package dev.johnshift.springboottdd.user;
 
-import lombok.Data;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 /** ... */
-@Data
+@Getter
+@Setter
+@Entity
 @Component
+@Table(name = "users")
 public class User {
+
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
+
+  @Column(name = "username")
   private String username;
-  private String password;
-  private String firstName;
-  private String lastName;
-  private String description;
+
+  @Column(name = "bio")
+  private String bio;
 }
--- src/main/java/dev/johnshift/springboottdd/user/UserController.java
+++ src/main/java/dev/johnshift/springboottdd/user/UserController.java
@@ -1,9 +1,23 @@
 package dev.johnshift.springboottdd.user;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /** ... */
 @RestController
 public class UserController {
 
+  @Autowired
+  UserRepository repo;
+
+  /** ... */
+  @GetMapping("/users")
+  public List<User> getUsersHandler() {
+
+    return repo.findAll();
+
+  }
 }
