8b997cd106a9e4380732474b092a457802b8eee8
John Ballesteros <johnshift.dev@gmail.com>
2021-09-27 19:09:48 +0800

    implement other crud controllers

--- src/main/java/dev/johnshift/springboottdd/user/UserController.java
+++ src/main/java/dev/johnshift/springboottdd/user/UserController.java
@@ -6,9 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,5 +45,23 @@ public class UserController {
     UserDTO createdUser = svc.createUser(user);
 
     return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+  }
+
+  /** ... */
+  @DeleteMapping("/users/{id}")
+  public ResponseEntity<UserDTO> handleDeleteUser(@PathVariable(name = "id") long id) {
+
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  /** 
+   * Only `bio` field should be updateable. 
+   */
+  @PutMapping("/users")
+  public ResponseEntity<UserDTO> handleUpdateUser(@RequestBody UserDTO user) {
+
+    UserDTO updatedUser = svc.updateUser(user);
+
+    return new ResponseEntity<>(updatedUser, HttpStatus.OK);
   }
 }
--- src/main/java/dev/johnshift/springboottdd/user/UserService.java
+++ src/main/java/dev/johnshift/springboottdd/user/UserService.java
@@ -1,6 +1,7 @@
 package dev.johnshift.springboottdd.user;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Service;
@@ -26,7 +27,6 @@ public class UserService {
 
   /** . */
   public UserDTO createUser(UserDTO userDTO) {
-
     
     UserEntity userEntity = userRepository.save(mapper.toEntity(userDTO));
 
@@ -39,7 +39,39 @@ public class UserService {
     UserEntity userEntity = userRepository.findById(id).orElseThrow();
 
     return mapper.toDTO(userEntity);
+  }
 
+  /** . */
+  public void deleteUserById(long id) {
 
+    if (!userExists(id)) {
+      throw new NoSuchElementException();
+    }
+
+    userRepository.deleteById(id);
+  }
+
+  /** 
+   * Only `bio` field should be updateable.
+   */
+  public UserDTO updateUser(UserDTO userDTO) {
+
+    long id = userDTO.getId();
+
+    if (!userExists(id)) {
+      throw new NoSuchElementException();
+    }
+
+    UserEntity userEntity = userRepository.findById(id).orElseThrow();
+    userEntity.setBio(userDTO.getBio());
+
+    userRepository.save(userEntity);
+
+    return mapper.toDTO(userEntity);
+  }
+
+  /** . */
+  public boolean userExists(long id) {
+    return userRepository.findById(id).isPresent();
   }
 }
