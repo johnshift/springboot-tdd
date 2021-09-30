4c215bf884c27563c2308ed812eaf5561e6b227c
John Ballesteros <johnshift.dev@gmail.com>
2021-09-30 16:07:05 +0800

    fix previous build

--- src/test/java/dev/johnshift/springboottdd/AppTest.java
+++ src/test/java/dev/johnshift/springboottdd/AppTest.java
@@ -3,10 +3,8 @@ package dev.johnshift.springboottdd;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class AppIT {
+class AppTest {
 
   /** 
    * Nothing fancy here. 
